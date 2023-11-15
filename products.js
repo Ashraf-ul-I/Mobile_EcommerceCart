435be4f17f24a61dcb7c3f85b10db33817dd3855
Md.Ashraful Islam <crackthecode10@gmail.com>
2023-11-15 21:38:51 +0600

    adding all the file of cart codes

---
+++ products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    id: 0,
+    name: "Iphone 11",
+    price: 700,
+    instock: 30,
+    description:
+      "Great Display with Good camera and Powerful bionic 11 chipset",
+    imgSrc: "./img/iphone-11-4gb-ram-128gb-.jpg",
+  },
+  {
+    id: 1,
+    name: "Iphone X",
+    price: 400,
+    instock: 43,
+    description:
+      "Its Introduce With a new notch and New Face Lock",
+    imgSrc: "./img/iphone.jpg",
+  },
+  {
+    id: 2,
+    name: "Pixel 7pro",
+    price: 800,
+    instock: 30,
+    description:
+      "King of a camera to this era with great ai feature Tensor g2 chipset",
+    imgSrc: "./img/pixel7.png",
+  },
+  {
+    id: 3,
+    name: "Samsung Galaxy S23 Ultra",
+    price: 1250,
+    instock: 5,
+    description:
+      "Its Provide 100x Clear zoom With magnificent Camera exist in the world",
+    imgSrc: "./img/galaxys2.jpg",
+  },
+  {
+    id: 4,
+    name: "Pixel 7",
+    price: 650,
+    instock: 5,
+    description:
+      "Best Camera in cheapest Price",
+    imgSrc: "./img/google-pixel7-pro-3-1-.jpg",
+  },
+  {
+    id: 5,
+    name: "Samsung Galaxy A54",
+    price: 450,
+    instock: 40,
+    description:
+      "This is a Great phone within the budget",
+    imgSrc: "./img/samsung.jpg",
+  },
+];
